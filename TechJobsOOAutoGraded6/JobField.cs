a3b33125e85566af5664fb005d6d1934cdf02156
Jacob Dayhoff <dayhoff@gmail.com>
2023-04-20 13:26:06 -0500

    Task 5 work

--- TechJobsOOAutoGraded6/JobField.cs
+++ TechJobsOOAutoGraded6/JobField.cs
@@ -1,28 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace TechJobsOOAutoGraded6
 {
-	public class Location
-	{
+    public abstract class JobField
+    {
         public int Id { get; }
         private static int nextId = 1;
         public string Value { get; set; }
 
-        public Location()
+        public JobField()
         {
             Id = nextId;
             nextId++;
         }
 
-        // TODO: Task 2: Add a second constructor to this class that uses the Location() constructor and sets the value of the value field.
-        public Location(string value) : this()
+        public JobField(string value) : this()
         {
             Value = value;
         }
 
         public override bool Equals(object obj)
         {
-            return obj is Location location &&
-                   Id == location.Id;
+            return obj is JobField jobField &&
+                   Id == jobField.Id;
         }
 
         public override int GetHashCode()
@@ -36,4 +40,3 @@ namespace TechJobsOOAutoGraded6
         }
     }
 }
-
